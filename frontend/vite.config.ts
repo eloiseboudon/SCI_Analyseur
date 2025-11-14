3b2acd77c6608cbc55a59bf136f6d7469b790c21
eloiseboudon <boudon.eloise@gmail.com>
2025-11-14 17:25:59 +0100

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5010',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
   },
 });
